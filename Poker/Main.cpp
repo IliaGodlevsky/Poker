fca7db9fb184ca7f2202b1221ea9d672f4ea8a70
ILya Hadleuski <48137948+IliaGodlevsky@users.noreply.github.com>
2021-02-18 10:12:54 +0300

    Update Main.cpp

--- Poker/Main.cpp
+++ Poker/Main.cpp
@@ -7,12 +7,12 @@ int main()
 	std::srand((unsigned)std::time(nullptr));
 	Deck deck;
 	deck.shuffle();
-	const int CARDS_TO_TAKE = 45;
+	const int CARDS_TO_TAKE = 52;
 	auto cards = deck.getCards(CARDS_TO_TAKE);
 
 	for (int i = 0; i < CARDS_TO_TAKE; i++)
 	{
-		if (i % 5 == 0 && i != 0)
+		if (i % 4 == 0 && i != 0)
 			std::cout << std::endl;
 		std::cout << *cards[i];
 	}
