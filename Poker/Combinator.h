3bb0ad346b2d385ed5d3435378b1464531b716c5
ILya Hadleuski <48137948+IliaGodlevsky@users.noreply.github.com>
2021-02-23 09:51:52 +0300

    Update Combinator.h

--- Poker/Combinator.h
+++ Poker/Combinator.h
@@ -1,20 +1,14 @@
 #ifndef COMBINATOR_H_
 #define COMBINATOR_H_
 
-#include "Pair.h"
-#include "TwoPairs.h"
-#include "Trips.h"
-#include "Straight.h"
-#include "Flush.h"
-#include "FullHouse.h"
-#include "FourOfAKind.h"
-#include "StraightFlush.h"
+#include "Combination.h"
 
 class Combinator
 {
 public:
 	Combinator();
-	const Combination* getCombo(const Cards&, const Hand&)const;
+	const Combination* getCombo(const Cards&, 
+		const Hand&)const;
 	~Combinator();
 private:
 	const int COMBOS;
